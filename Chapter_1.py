54a90d527ecc7bb19883893a73ce1d996d78fdc9
WillieYang <849346604@qq.com>
2017-04-12 11:28:49 +0100

    Self Check in 1.4.5

--- Chapter_1.py
+++ Chapter_1.py
@@ -1,10 +1,36 @@
-word_list = ['cat', 'dog', 'rabbit']
-letter_set = set([])
-for a_word in word_list:
-    for a_letter in a_word:
-        letter_set.add(a_letter)
-letter_list = list(letter_set)
-print(letter_list)
+import random
 
-letter_list = [a_letter for a_word in word_list for a_letter in a_word]
-print(list(set(letter_list)))
+letters_space = 'abcdefghijklmnopqrstuvwxyz '
+sentence_list = []
+sentence_shoot = 'methinks it is like a weasel'
+
+def iteration(arg):
+    if arg == False:
+        print("This time is failed, try again")
+        string_generate()
+    else:
+        print("Mokey Sucessful")
+
+def compare(arg):
+    if arg == sentence_shoot:
+        return iteration(True)
+    else:
+        iteration(False)
+
+def string_generate():
+    sentence_list[:] = []
+    for ch in range(27):
+        sentence_list.append(random.choice(letters_space))
+    sentence_string = ''.join(sentence_list)
+    compare(sentence_string)
+
+string_generate()
+
+
+
+# first step: generate sentence_string
+# second step: compare sentence_string to the sentence_shoot
+# thrid step: if True, print the sentence_shoot
+#               else return False
+# fourh step: iterate the above steps until the sentence_shoot is printed.
+# Self Check Challenge did not finish...
