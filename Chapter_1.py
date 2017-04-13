fbacc472c60bf842927c9e6af8852943acb42737
WillieYang <849346604@qq.com>
2017-04-13 10:21:51 +0100

    Programming exercise in 1.7.3

--- Chapter_1.py
+++ Chapter_1.py
@@ -25,6 +25,22 @@ class Fraction:
         new_den = self.den * other_fraction.den
         return Fraction(new_num, new_den)
 
+    def __sub__(self, other_fraction):
+        new_num = self.num * other_fraction.den - \
+                    self.den * other_fraction.num
+        new_den = self.den * other_fraction.den
+        return Fraction(new_num, new_den)
+
+    def __mul__(self, other_fraction):
+        new_num = self.num * other_fraction.num
+        new_den = self.den * other_fraction.den
+        return Fraction(new_num, new_den)
+
+    def __div__(self, other_fraction):
+        new_num = self.num * other_fraction.den
+        new_den = self.den * other_fraction.num
+        return Fraction(new_num, new_den)
+
     def __eq__(self, other):
         first_num = self.num * other.den
         second_num = other.num * self.den
@@ -43,6 +59,9 @@ x = Fraction(2, 4)
 y = Fraction(2, 3)
 
 print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
 print(x == y)
 
 print(x.get_num())
