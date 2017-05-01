9f8c711cdd89256faf2cad7cff6abdcc3c441d79
Sheng Yang <849346604@qq.com>
2017-05-01 10:21:58 +0100

    2.2.2 Anagram Detection Example Solution 2

--- Chapter_2.py
+++ Chapter_2.py
@@ -1,25 +1,20 @@
-def anagram_solution(s1, s2):
-    a_list = list (s2)
+def anagram_solution2(s1, s2):
+    a_list1 = list(s1)
+    a_list2 = list(s2)
 
-    pos1 = 0
-    still_ok = True
+    a_list1.sort()
+    a_list2.sort()
+    print(a_list1)
 
-    while pos1 < len(s1) and still_ok:
-        pos2 = 0
-        found = False
-        while pos2 < len(a_list) and not found:
-            if s1[pos1] == a_list[pos2]:
-                found = True
-            else:
-                pos2 = pos2 + 1
+    pos = 0
+    matches = True
 
-        if found:
-            a_list[pos2] = None
+    while pos < len(s1) and matches:
+        if a_list1[pos] == a_list2[pos]:
+            pos = pos + 1
         else:
-            still_ok = False
+            matches = False
 
-        pos1 = pos1 + 1
+    return matches
 
-    return still_ok
-
-print(anagram_solution('abcd', 'cdba'))
+print(anagram_solution2('abcde', 'ebcda'))
