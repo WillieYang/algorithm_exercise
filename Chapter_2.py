99e6d5609fc0c64a73c1e03f14881a97eb64cac2
Sheng Yang <849346604@qq.com>
2017-05-01 10:00:03 +0100

    2.2.2 Anagram Detection Example Solution 1

--- Chapter_2.py
+++ Chapter_2.py
@@ -1,10 +1,25 @@
-List = [3, 2, 5, 9, 7, 4, 20]
-for i in List:
-    print('i is %d' % i)
-    Min = i
-    for j in List:
-        print('j is %d' % j)
-        if j < Min:
-            Min = j
-            print('Min is %d' % Min)
-print('The minimum number is: %d' % Min)
+def anagram_solution(s1, s2):
+    a_list = list (s2)
+
+    pos1 = 0
+    still_ok = True
+
+    while pos1 < len(s1) and still_ok:
+        pos2 = 0
+        found = False
+        while pos2 < len(a_list) and not found:
+            if s1[pos1] == a_list[pos2]:
+                found = True
+            else:
+                pos2 = pos2 + 1
+
+        if found:
+            a_list[pos2] = None
+        else:
+            still_ok = False
+
+        pos1 = pos1 + 1
+
+    return still_ok
+
+print(anagram_solution('abcd', 'cdba'))
